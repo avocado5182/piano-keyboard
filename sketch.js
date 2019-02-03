1b8ecfb2fef837c9a21991bf5888a05855fe866e
avocado5182 <46830012+avocado5182@users.noreply.github.com>
2019-02-03 10:23:43 -0500

    z

--- sketch.js
+++ sketch.js
@@ -1,12 +1,31 @@
+var rectWidth;
+   
 function setup() {
-  createCanvas(640, 480);
+  createCanvas(720, 400);
+  noStroke();
+  background(230);
+  rectWidth = width/4;
 }
 
-function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255);
+function draw() { 
+  // keep draw() here to continue looping while waiting for keys
+}
+
+function keyPressed() {
+  var keyIndex = -1;
+  if (key >= 'a' && key <= 'z') {
+    keyIndex = key.charCodeAt(0) - 'a'.charCodeAt(0);
   }
-  ellipse(mouseX, mouseY, 80, 80);
+  if (keyIndex == -1) {
+    // If it's not a letter key, clear the screen
+    background(230);
+  } else { 
+    // It's a letter key, fill a rectangle
+    randFill_r = Math.floor(Math.random() * 255 + 1);
+    randFill_g = Math.floor(Math.random() * 255 + 1);
+    randFill_b = Math.floor(Math.random() * 255 + 1);
+    fill(randFill_r,randFill_g,randFill_b);
+    var x = map(keyIndex, 0, 25, 0, width - rectWidth);
+    rect(x, 0, rectWidth, height);
+  }
 }
