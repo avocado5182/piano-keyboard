5d0bdd6bb62c1edfca48f8a2d662879447e0d4fa
avocado5182 <46830012+avocado5182@users.noreply.github.com>
2019-02-03 10:17:39 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,12 @@
-function setup() { 
-  createCanvas(400, 400);
-} 
+function setup() {
+  createCanvas(640, 480);
+}
 
-function draw() { 
-  background(220);
-  ellipse(50,50,80,80);
+function draw() {
+  if (mouseIsPressed) {
+    fill(0,255,0);
+  } else {
+    fill(255,0,255);
+  }
+  ellipse(mouseX, mouseY, 80, 80);
 }
