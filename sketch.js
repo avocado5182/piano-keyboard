4d7c8060d27cb7cdd66544e8ee8829f3feecae77
avocado5182 <46830012+avocado5182@users.noreply.github.com>
2019-02-03 10:15:45 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,8 @@
-function setup() {
+function setup() { 
+  createCanvas(400, 400);
+} 
 
-}
-
-function draw() {
-  ellipse(50, 50, 80, 80);
+function draw() { 
+  background(220);
+  ellipse(50,50,80,80);
 }
